1f3f8afa9e4c7bc0509114e2991a4d0b651132d1
Dogogh <103599939+LucasQuadros19@users.noreply.github.com>
2025-05-31 20:23:36 -0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -32,7 +32,3 @@ variable "public_key_path" {
   description = "Caminho para a chave pública SSH"
   default     = "~/.ssh/id_rsa.pub"  
 }
-variable "credentials_file_path" {
-  description = "Caminho para o arquivo de credenciais do GCP"
-  type        = string
-}
