7c4fe29f7c955b0aa99f446f2b62ba3c9297e700
Dogogh <103599939+LucasQuadros19@users.noreply.github.com>
2025-06-01 17:29:21 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,10 @@
 provider "google" {
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
-  credentials = file(var.credentials_file_path) 
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+  credentials = file(var.credentials_file_path)
 }
+
 
 resource "google_compute_instance" "vm_instance" {
   name         = "dev-vm"
