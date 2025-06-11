f95e9469ed6fbd8dcb655f7f41bd026a56d0f6cb
Dogogh <103599939+LucasQuadros19@users.noreply.github.com>
2025-06-11 19:57:09 -0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,19 +9,37 @@ variable "zone" {
   default     = "us-central1-a"
 }
 
-
 variable "machine_type" {
   description = "Tipo da máquina para os nós do cluster"
+  type        = string
   default     = "e2-small"
 }
+
 variable "region" {
   description = "Região da GCP (ex: us-central1)"
   type        = string
   default     = "us-west1"
 }
 
-
 variable "public_key_path" {
   description = "Caminho para a chave pública SSH"
+  type        = string
   default     = "~/.ssh/id_rsa.pub"
+}
+
+variable "image" {
+  description = "Imagem do sistema operacional para a VM"
+  type        = string
+  default     = "ubuntu-os-cloud/ubuntu-2204-lts" 
+}
+
+variable "disk_size" {
+  description = "Tamanho do disco da VM"
+  type        = number
+  default     = 10  
+}
+
+variable "credentials_file_path" {
+  description = "Caminho para o arquivo de credenciais do GCP"
+  type        = string
 }
