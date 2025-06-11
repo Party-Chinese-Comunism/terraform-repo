fb4de76a5ea0a57d700072ee1dafa290a2331bbc
Dogogh <103599939+LucasQuadros19@users.noreply.github.com>
2025-06-11 20:53:15 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file(var.credentials_file)
+  credentials = file(var.credentials_file_path)
   project     = var.project_id
   region      = var.region
   zone        = var.zone
