8f9541f00b7e1ed64d11856f0fea9eb36b6e69d4
AndreGChang <FireWitch2003@outlook.com>
2025-06-11 23:58:28 -0300

    a

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,41 +8,8 @@ resource "google_container_cluster" "primary" {
   name     = "cluster-hml"
   location = var.zone
 
-  remove_default_node_pool = true
-  initial_node_count       = 1 
+  enable_autopilot = true  # Habilita o modo Autopilot
 
   network    = "default"
   subnetwork = "default"
-
 }
-
-resource "google_container_node_pool" "primary_nodes" {
-  name     = "primary-node-pool"
-  location = var.zone
-  cluster  = google_container_cluster.primary.name
-
-  node_config {
-    machine_type = var.machine_type
-    disk_size_gb = 10
-    disk_type    = "pd-standard"
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform",
-    ]
-  }
-
-  initial_node_count = 1
-}
-
-
-# resource "google_compute_firewall" "allow_ssh_http" {
-#   name    = "allow-ssh-http"
-#   network = "default"
-#
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["22", "80", "3000", "8000", "9090", "5000"]
-#   }
-#
-#   source_ranges = ["0.0.0.0/0"]
-#   target_tags   = ["ssh", "app"]
-# }
