7a33c4220c1c34638a0e36b22a5b342bc7690912
AndreGChang <FireWitch2003@outlook.com>
2025-06-12 10:31:43 -0300

    hotfix: ajuste no cluster

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,45 +1,12 @@
 variable "project_id" {
-  description = "O ID do projeto GCP"
-  type        = string
-}
-
-variable "zone" {
-  description = "Zona da GCP (ex: us-central1-a)"
-  type        = string
-  default     = "us-central1-b"
-}
-
-variable "machine_type" {
-  description = "Tipo da máquina para os nós do cluster"
-  type        = string
-  default     = "e2-small"
+  type = string
 }
 
 variable "region" {
-  description = "Região da GCP (ex: us-central1)"
-  type        = string
-  default     = "us-central1"
-}
-
-variable "public_key_path" {
-  description = "Caminho para a chave pública SSH"
-  type        = string
-  default     = "~/.ssh/id_rsa.pub"
-}
-
-variable "image" {
-  description = "Imagem do sistema operacional para a VM"
-  type        = string
-  default     = "ubuntu-os-cloud/ubuntu-2204-lts" 
-}
-
-variable "disk_size" {
-  description = "Tamanho do disco da VM"
-  type        = number
-  default     = 10  
+  type    = string
+  default = "us-east1"
 }
 
 variable "credentials_file_path" {
-  description = "Caminho para o arquivo de credenciais do GCP"
-  type        = string
+  type = string
 }
