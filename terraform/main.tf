9f3b31524479e8c74c609ffa48faa517a4c524d2
Dogogh <103599939+LucasQuadros19@users.noreply.github.com>
2025-06-13 14:32:54 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,27 +2,13 @@ provider "google" {
   credentials = file(var.credentials_file_path)
   project     = var.project_id
   region      = var.region
-  zone        = var.zone
 }
 
 resource "google_container_cluster" "primary" {
-  name     = "meu-cluster-gke-autopilot"
-  location = var.zone
+  name               = "cluster-hml-central"
+  location           = var.region
+  enable_autopilot   = true
 
-
-  initial_node_count = 1 
-  remove_default_node_pool = true  
-
-  # Rede e sub-rede onde o cluster será criado (se necessário, altere conforme sua configuração de rede)
   network    = "default"
   subnetwork = "default"
-
-}
-
-output "cluster_name" {
-  value = google_container_cluster.primary.name
-}
-
-output "cluster_endpoint" {
-  value = google_container_cluster.primary.endpoint
 }
