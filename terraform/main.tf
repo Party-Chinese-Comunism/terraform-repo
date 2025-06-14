b7700c34e1156f8cc84baad32697f74044175822
Dogogh <103599939+LucasQuadros19@users.noreply.github.com>
2025-06-14 18:30:01 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,13 @@ provider "google" {
   region      = var.region
 }
 
+# Criação do IP Estático
+resource "google_compute_address" "ingress_ip" {
+  name   = "ingress-static-ip"
+  region = var.region
+}
+
+# Criação do Cluster GKE
 resource "google_container_cluster" "primary" {
   name               = "cluster-prod-central"
   location           = var.region
@@ -11,4 +18,27 @@ resource "google_container_cluster" "primary" {
 
   network    = "default"
   subnetwork = "default"
+
+  node_pool {
+    name       = "default-node-pool"
+    location   = var.region
+    initial_node_count = 3
+    node_config {
+      machine_type = "e2-medium"
+    }
+
+    # Usando tags para habilitar tráfego HTTP e HTTPS
+    tags = ["http-server", "https-server"]
+  }
+
+
+  endpoint = google_compute_address.ingress_ip.address
+}
+
+output "ingress_ip_address" {
+  value = google_compute_address.ingress_ip.address
+}
+
+output "cluster_name" {
+  value = google_container_cluster.primary.name
 }
