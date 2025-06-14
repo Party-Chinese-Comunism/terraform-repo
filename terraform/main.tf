9db9f5c13ae0c69830926d257584089eb1ac8dc3
AndreGChang <FireWitch2003@outlook.com>
2025-06-14 12:41:51 -0300

    a10

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,22 +4,26 @@ provider "google" {
   region      = var.region
 }
 
+# Cluster GKE com Autopilot ativado
 resource "google_container_cluster" "primary" {
-  name               = "cluster-hml-central"
-  location           = var.region
-  enable_autopilot   = true
+  name             = "cluster-hml-central"
+  location         = var.region
+  enable_autopilot = true
 
   network    = "default"
   subnetwork = "default"
 }
 
+# Coleta token de autenticação para o provider helm
 data "google_client_config" "default" {}
 
+# IP fixo para uso com o Ingress NGINX
 resource "google_compute_address" "ingress_ip" {
   name   = "ingress-static-ip"
   region = var.region
 }
 
+# Provedor Helm (só funciona após o cluster existir)
 provider "helm" {
   kubernetes {
     host                   = google_container_cluster.primary.endpoint
@@ -28,8 +32,9 @@ provider "helm" {
   }
 }
 
-
+# Instalação do Ingress NGINX, protegida com count para evitar execução antes do cluster
 resource "helm_release" "nginx_ingress" {
+  count            = var.enable_ingress ? 1 : 0
   name             = "ingress-nginx"
   chart            = "ingress-nginx"
   repository       = "https://kubernetes.github.io/ingress-nginx"
