388133f60f8e959763f18ca327a108d4948a0a5c
Dogogh <103599939+LucasQuadros19@users.noreply.github.com>
2025-06-14 19:02:00 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,26 +5,20 @@ provider "google" {
 }
 
 resource "google_container_cluster" "primary" {
-  name               = "cluster-prod-central"
-  location           = var.region
-  enable_autopilot   = true
+  name             = "cluster-prod-central"
+  region           = var.region  
+  enable_autopilot = true
 
   network    = "default"
   subnetwork = "default"
 
   node_pool {
     name               = "default-node-pool"
-    location           = var.region
     initial_node_count = 1
     node_config {
       machine_type = "e2-medium"
     }
-
-   
-    tags = ["http-server", "https-server"]
   }
-
- 
 }
 
 
