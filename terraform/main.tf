368275c8b00b5194a272f5c00438d1cf642f108e
AndreGChang <FireWitch2003@outlook.com>
2025-06-16 21:53:27 -0300

    a24

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,3 +17,76 @@ resource "google_container_cluster" "primary" {
   network    = "default"
   subnetwork = "default"
 }
+
+# Observabilidade: Prometheus + Grafana com Ingress NGINX
+resource "helm_release" "kube_prometheus_stack" {
+  name             = "observability"
+  namespace        = "monitoring"
+  create_namespace = true
+
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  version          = "57.0.2"
+
+  set {
+    name  = "grafana.adminPassword"
+    value = "admin123"
+  }
+
+  set {
+    name  = "grafana.service.type"
+    value = "ClusterIP"
+  }
+
+  set {
+    name  = "grafana.ingress.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "grafana.ingress.path"
+    value = "/grafana"
+  }
+
+  set {
+    name  = "grafana.ingress.ingressClassName"
+    value = "nginx"
+  }
+
+  set {
+    name  = "grafana.ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/rewrite-target"
+    value = "/$1"
+  }
+
+  set {
+    name  = "grafana.ingress.pathType"
+    value = "ImplementationSpecific"
+  }
+
+  set {
+    name  = "prometheus.ingress.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "prometheus.ingress.path"
+    value = "/prometheus"
+  }
+
+  set {
+    name  = "prometheus.ingress.ingressClassName"
+    value = "nginx"
+  }
+
+  set {
+    name  = "prometheus.ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/rewrite-target"
+    value = "/$1"
+  }
+
+  set {
+    name  = "prometheus.ingress.pathType"
+    value = "ImplementationSpecific"
+  }
+
+  depends_on = [google_container_cluster.primary]
+}
