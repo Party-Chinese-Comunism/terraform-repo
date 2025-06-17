c2896fbd04156719fd513fff136a8639c5280498
AndreGChang <FireWitch2003@outlook.com>
2025-06-16 23:50:28 -0300

    33

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,14 +28,24 @@ resource "google_container_cluster" "primary" {
   subnetwork = "default"
 }
 
+data "google_client_config" "default" {}
+
+provider "helm" {
+  kubernetes {
+    host                   = google_container_cluster.primary.endpoint
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  }
+}
+
 resource "helm_release" "kube_prometheus_stack" {
   name             = "observability"
   namespace        = "monitoring"
   create_namespace = true
 
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "kube-prometheus-stack"
-  version    = "57.0.2"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  version          = "57.0.2"
 
   set {
     name  = "grafana.adminPassword"
@@ -50,47 +60,6 @@ resource "helm_release" "kube_prometheus_stack" {
   set {
     name  = "prometheus.service.type"
     value = "LoadBalancer"
-  }
-
-  # GKE Autopilot-safe configuration
-  set {
-    name  = "defaultRules.create"
-    value = "false"
-  }
-
-  set {
-    name  = "kubelet.enabled"
-    value = "false"
-  }
-
-  set {
-    name  = "coreDns.enabled"
-    value = "false"
-  }
-
-  set {
-    name  = "nodeExporter.enabled"
-    value = "false"
-  }
-
-  set {
-    name  = "kubeControllerManager.enabled"
-    value = "false"
-  }
-
-  set {
-    name  = "kubeScheduler.enabled"
-    value = "false"
-  }
-
-  set {
-    name  = "kubeProxy.enabled"
-    value = "false"
-  }
-
-  set {
-    name  = "kubeEtcd.enabled"
-    value = "false"
   }
 
   depends_on = [google_container_cluster.primary]
