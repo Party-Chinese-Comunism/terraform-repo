52bd49058717960bde10872d56ef37ec8529f7e4
Dogogh <103599939+LucasQuadros19@users.noreply.github.com>
2025-06-17 01:37:46 -0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
-
 output "cluster_endpoint" {
-  value = google_container_cluster.cluster.endpoint
+  description = "Endpoint do cluster GKE"
+  value       = google_container_cluster.primary.endpoint
 }
