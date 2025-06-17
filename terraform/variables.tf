55f7e11b11c6bd9477c1f75c6fa619235dd6054b
Dogogh <103599939+LucasQuadros19@users.noreply.github.com>
2025-06-16 23:14:23 -0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,7 +10,7 @@ variable "region" {
 variable "credentials_file_path" {
   type = string
 }
-variable "static_ip" {
-  description = "O endereço IP estático a ser usado no cluster"
-  type        = string
-}
+#variable "static_ip" {
+ # description = "O endereço IP estático a ser usado no cluster"
+ # type        = string
+#}
