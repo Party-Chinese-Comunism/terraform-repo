762c7b54f35e356c428367b48928d96dfaa1b256
Dogogh <103599939+LucasQuadros19@users.noreply.github.com>
2025-06-16 23:10:54 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,8 +11,6 @@ resource "google_container_cluster" "primary" {
 
   network    = "default"
   subnetwork = "default"
-
-
 }
 
 output "cluster_name" {
@@ -21,4 +19,44 @@ output "cluster_name" {
 
 output "ingress_ip_address" {
   value = var.static_ip
+}
+
+# Configuração do provedor do Google
+data "google_client_config" "default" {}
+
+# Configuração do provedor Helm para Kubernetes
+provider "helm" {
+  kubernetes {
+    host                   = google_container_cluster.primary.endpoint
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  }
+}
+
+# Release do Grafana e Prometheus
+resource "helm_release" "kube_prometheus_stack" {
+  name             = "observability"
+  namespace        = "monitoring"
+  create_namespace = true
+
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  version          = "57.0.2"
+
+  set {
+    name  = "grafana.adminPassword"
+    value = "admin123"
+  }
+
+  set {
+    name  = "grafana.service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
+    name  = "prometheus.service.type"
+    value = "LoadBalancer"
+  }
+
+  depends_on = [google_container_cluster.primary]
 }
