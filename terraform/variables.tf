ac7feeb37f9eb8b05dc5609abf4ca315dee76d51
AndreGChang <FireWitch2003@outlook.com>
2025-06-17 01:20:31 -0300

    38

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,45 +1,47 @@
 variable "project_id" {
-  type        = string
-  description = "ID do projeto GCP"
+  type = string
 }
 
 variable "cluster_name" {
-  type  = string
-  description = "nome do cluster"
+  type    = string
+  default = "cluster-hml-central"
 }
 
 variable "cluster_size" {
-  type = string
-  description = "tamanho do cluster"
+  type    = number
+  default = 1
 }
 
 variable "enable_ingress" {
   type    = bool
-  default = false
+  default = true
 }
 
 variable "bucket_name" {
-  type = string
-  description = "nome do bucket"
+  type    = string
+  default = "blog-iris-hml"
 }
 
 variable "region" {
+  type    = string
   default = "us-east1"
 }
 
 variable "zone" {
-  default = "us-east1-a"
+  type    = string
+  default = "us-east1-b"
 }
 
 variable "machine_type" {
+  type    = string
   default = "e2-medium"
 }
 
 variable "image" {
-  default = "ubuntu-os-cloud/ubuntu-2204-lts"
+  type    = string
+  default = "cos_containerd"
 }
 
 variable "credentials_file_path" {
-  description = "Caminho para o arquivo de credenciais do Google Cloud"
-  type        = string
+  type = string
 }
