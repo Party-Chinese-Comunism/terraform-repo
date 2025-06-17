03a0d6b523a460ea9cfc91dcfe8d6001842f12fd
Dogogh <103599939+LucasQuadros19@users.noreply.github.com>
2025-06-17 00:49:44 -0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,40 @@
 variable "project_id" {
+  type        = string
+  description = "ID do projeto GCP"
+}
+
+variable "cluster_name" {
+  type  = string
+  description = "nome do cluster"
+}
+
+variable "cluster_size" {
   type = string
+  description = "tamanho do cluster"
+}
+
+variable "bucket_name" {
+  type = string
+  description = "nome do bucket"
 }
 
 variable "region" {
-  type    = string
   default = "us-central1"
 }
 
-variable "credentials_file_path" {
-  type = string
+variable "zone" {
+  default = "us-central1-a"
 }
-#variable "static_ip" {
- # description = "O endereço IP estático a ser usado no cluster"
- # type        = string
-#}
+
+variable "machine_type" {
+  default = "e2-medium"
+}
+
+variable "image" {
+  default = "ubuntu-os-cloud/ubuntu-2204-lts"
+}
+
+variable "credentials_file_path" {
+  description = "Caminho para o arquivo de credenciais do Google Cloud"
+  type        = string
+}
