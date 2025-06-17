287c005da81ed529058651d62cbbf1db4a3f97d4
AndreGChang <FireWitch2003@outlook.com>
2025-06-16 22:27:50 -0300

    a26

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,8 +12,6 @@ provider "helm" {
     token                  = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
   }
-
-  depends_on = [google_container_cluster.primary]
 }
 
 data "google_compute_address" "ingress_ip" {
